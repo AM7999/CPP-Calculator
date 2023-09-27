328478dca078cfd1d3be05936457feea227f4a68
Angel <AM799912122019@gmail.com>
2023-09-26 19:25:42 -0700

    yay
    Signed-off-by: Angel <AM799912122019@gmail.com>

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 int main() {
 	//Variables-Declare
-	int launch;
 	int menu;
 	char relaunch;
 	int addOne;
@@ -16,20 +15,9 @@ int main() {
 	float divOne;
 	float divTwo;
 
-	cout << "Please select the operating system you are using" << endl;
-	cout << "1. Windows" << endl;
-	cout << "2. Linux" << endl;
-	cin >> launch;
 
-	if (launch == 1) {
-		system("cls");
-	}
 
-	if (launch == 2) {
-		system("clear");
-	}
-
-	init:
+init:
 	cout << "___________________________" << endl;
 	cout << "|Please Select An Option  |" << endl;
 	cout << "|                         |" << endl;
@@ -42,205 +30,123 @@ int main() {
 	cout << "Option: ";
 	cin >> menu;
 
-	if (launch == 1) {
-		if (menu == 1) {
-			system("cls");
-			cout << "Please Select a number for the variable addOne: ";
-			cin >> addOne;
-			cout << "Please Select a number for the variable AddTwo: ";
-			cin >> addTwo;
-			cout << "Result: " << addOne + addTwo << endl;
-			cout << "Do you want to re-run the program? Y/N ";
-			cin >> relaunch;
-			if (relaunch == 'Y') {
-				goto init;
-			}
-			if (relaunch == 'N') {
-				cout << "Saving history to file.." << endl;
-				ofstream logfile;
-				logfile.open("log.txt");
-				logfile << "FILE WILL BE ERASED ON PROGRAM EXIT \n Addition: "; 
-				logfile << addOne + addTwo;
-				logfile.close(); 
-				cout << "Exit.." << endl;
-				return 2;
-			}
-			else {
-				cout << "Expected uppercase got: " << relaunch << endl;
-				return -1;
-			}
-		}
 
-		if (menu == 2) {
-			system("cls");
-			cout << "Please Select a number for the variable subOne: ";
-			cin >> subOne;
-			cout << "Please Select a number for the variable subTwo: ";
-			cin >> subTwo;
-			cout << "Result: " << subOne - subTwo << endl;
-			cout << "Do you want to re-run the program? Y/N ";
-			cin >> relaunch;
-			if (relaunch == 'Y') {
-				goto init;
-			}
-			if (relaunch == 'N') {
-				cout << "Exit.." << endl;
-				return 2;
-			}
-			else {
-				cout << "Expected uppercase got: " << relaunch << endl;
-				return -1;
-			}
+	if (menu == 1) {
+		system("cls");
+		system("clear");
+		cout << "Please Select a number for the variable addOne: ";
+		cin >> addOne;
+		cout << "Please Select a number for the variable AddTwo: ";
+		cin >> addTwo;
+		cout << "Result: " << addOne + addTwo << endl;
+		cout << "Do you want to re-run the program? Y/N ";
+		cin >> relaunch;
+		if (relaunch == 'Y') {
+			goto init;
 		}
-
-		if (menu == 3) {
-			system("cls");
-			cout << "Please Select a number for the variable mulOne: ";
-			cin >> mulOne;
-			cout << "Please Select a number for the variable mulTwo: ";
-			cin >> mulTwo;
-			cout << "Result: " << mulOne * mulTwo;
-			cout << "Do you want to re-run the program? Y/N " << endl;
-			cin >> relaunch;
-			if (relaunch == 'Y') {
-				goto init;
-			}
-			if (relaunch == 'N') {
-				cout << "Exit.." << endl;
-				return 2;
-			}
-			else {
-				cout << "Expected uppercase got: " << relaunch << endl;
-				return -1;
-			}
+		if (relaunch == 'N') {
+			cout << "Saving history to file.." << endl;
+			ofstream logfile;
+			logfile.open("log.txt");
+			logfile << "FILE WILL BE ERASED ON PROGRAM EXIT \n Addition: ";
+			logfile << addOne + addTwo;
+			logfile.close();
+			cout << "Exit.." << endl;
+			return 2;
 		}
-
-		if (menu == 4) {
-			system("cls");
-			cout << "Please Select a number for the variable divOne: ";
-			cin >> divOne;
-			cout << "Please Select a number for the variable divTwo: ";
-			cin >> divTwo;
-			cout << "Result: " << divOne / divTwo << endl;
-			cout << "Do you want to re-run the program? Y/N " << endl;
-			cin >> relaunch;
-			if (relaunch == 'Y') {
-				goto init;
-			}
-			if (relaunch == 'N') {
-				cout << "Exit.." << endl;
-				return 2;
-			}
-			else {
-				cout << "Expected uppercase got: " << relaunch << endl;
-				return -1;
-			}
+		else {
+			cout << "Expected uppercase got: " << relaunch << endl;
+			return -1;
 		}
 	}
-	
-	if (launch == 2) {
-		if (menu == 1) {
-			system("clear");
-			cout << "Please Select a number for the variable addOne: ";
-			cin >> addOne;
-			cout << "Please Select a number for the variable AddTwo: ";
-			cin >> addTwo;
-			cout << "Result: " << addOne + addTwo << endl;
-			cout << "Do you want to re-run the program? Y/N ";
-			cin >> relaunch;
-			if (relaunch == 'Y') {
-				goto init;
-			}
-			if (relaunch == 'N') {
-				cout << "Exit.." << endl;
-				return 2;
-			}
-			else {
-				cout << "Expected uppercase got: " << relaunch << endl;
-				return -1;
-			}
-		}
 
-		if (menu == 2) {
-			system("clear");
-			cout << "Please Select a number for the variable subOne: ";
-			cin >> subOne;
-			cout << "Please Select a number for the variable subTwo: ";
-			cin >> subTwo;
-			cout << "Result: " << subOne - subTwo << endl;
-			cout << "Do you want to re-run the program? Y/N ";
-			cin >> relaunch;
-			if (relaunch == 'Y') {
-				goto init;
-			}
-			if (relaunch == 'N') {
-				cout << "Exit.." << endl;
-				return 2;
-			}
-			else {
-				cout << "Expected uppercase got: " << relaunch << endl;
-				return -1;
-			}
+	if (menu == 2) {
+		system("cls");
+		system("clear");
+		cout << "Please Select a number for the variable subOne: ";
+		cin >> subOne;
+		cout << "Please Select a number for the variable subTwo: ";
+		cin >> subTwo;
+		cout << "Result: " << subOne - subTwo << endl;
+		cout << "Do you want to re-run the program? Y/N ";
+		cin >> relaunch;
+		if (relaunch == 'Y') {
+			goto init;
 		}
-
-		if (menu == 3) {
-			system("clear");
-			cout << "Please Select a number for the variable mulOne: ";
-			cin >> mulOne;
-			cout << "Please Select a number for the variable mulTwo: ";
-			cin >> mulTwo;
-			cout << "Result: " << mulOne * mulTwo;
-			cout << "Do you want to re-run the program? Y/N ";
-			cin >> relaunch;
-			if (relaunch == 'Y') {
-				goto init;
-			}
-			if (relaunch == 'N') {
-				cout << "Exit.." << endl;
-				return 2;
-			}
-			else {
-				cout << "Expected uppercase got: " << relaunch << endl;
-				return -1;
-			}
+		if (relaunch == 'N') {
+			cout << "Saving history to file.." << endl;
+			ofstream logfile;
+			logfile.open("log.txt");
+			logfile << "FILE WILL BE ERASED ON PROGRAM EXIT \n Subtraction: ";
+			logfile << subOne - subTwo;
+			logfile.close();
+			cout << "Exit.." << endl;
+			return 2;
 		}
-
-		if (menu == 4) {
-			system("clear");
-			cout << "Please Select a number for the variable divOne: ";
-			cin >> divOne;
-			cout << "Please Select a number for the variable divTwo: ";
-			cin >> divTwo;
-			cout << "Result: " << divOne / divTwo << endl;
-			cout << "Do you want to re-run the program? Y/N ";
-			cin >> relaunch;
-			if (relaunch == 'Y') {
-				goto init;
-			}
-			if (relaunch == 'N') {
-				cout << "Exit.." << endl;
-				return 2;
-			}
-			else {
-				cout << "Expected uppercase got: " << relaunch << endl;
-				return -1;
-			}
+		else {
+			cout << "Expected uppercase got: " << relaunch << endl;
+			return -1;
 		}
 	}
+
+	if (menu == 3) {
+		system("cls");
+		system("clear");
+		cout << "Please Select a number for the variable mulOne: ";
+		cin >> mulOne;
+		cout << "Please Select a number for the variable mulTwo: ";
+		cin >> mulTwo;
+		cout << "Result: " << mulOne * mulTwo;
+		cout << "Do you want to re-run the program? Y/N " << endl;
+		cin >> relaunch;
+		if (relaunch == 'Y') {
+			goto init;
+		}
+		if (relaunch == 'N') {
+			cout << "Exit.." << endl;
+			return 2;
+		}
+		else {
+			cout << "Expected uppercase got: " << relaunch << endl;
+			return -1;
+		}
+	}
+
+	if (menu == 4) {
+		system("cls");
+		system("clear");
+		cout << "Please Select a number for the variable divOne: ";
+		cin >> divOne;
+		cout << "Please Select a number for the variable divTwo: ";
+		cin >> divTwo;
+		cout << "Result: " << divOne / divTwo << endl;
+		cout << "Do you want to re-run the program? Y/N " << endl;
+		cin >> relaunch;
+		if (relaunch == 'Y') {
+			goto init;
+		}
+		if (relaunch == 'N') {
+			cout << "Exit.." << endl;
+			return 2;
+		}
+		else {
+			cout << "Expected uppercase got: " << relaunch << endl;
+			return -1;
+		}
+	}
+
+
 
 	if (menu == 5) {
-		return 2;
+		goto exit;
 	}
-	
+
 	if (menu >> 5); {
-		if (launch == 1) {
-			system("cls");
-		}
-		if (launch == 2) {
-			system("clear");
-		}
+		system("cls");
+		system("clear");
 		goto init;
 	}
 
+	exit:
 	return 0;
 }
